4c47c1a3b5e29a29b52c76ddfcd0b9aca7c9dcee
Kevin_Garcia <kevin.garcias@utem.cl>
2020-09-16 23:29:41 -0300

    beta_grabacion1

--- Slider.m
+++ Slider.m
@@ -96,7 +96,6 @@ end
 % --- Executes during object creation, after setting all properties.
 function axes1_CreateFcn(hObject, eventdata, handles)
 
-%%%%%%%%%%%%%%%%%%%AQUI COMIENZA EL CODIGO%%%%%%%%%%%%%%%%%%%%%
 
 % --- Executes on button press in pushbutton1.
 function pushbutton1_Callback(hObject, eventdata, handles)
@@ -232,90 +231,168 @@ for i=1:np
     end
 end
 
-        DD(1)=dd;
-        % Theta(1)=theta;
-        Gamma(1)=gamma0;
-        Lambda(1)=lambda0;
+DD(1)=dd;
+% Theta(1)=theta;
+Gamma(1)=gamma0;
+Lambda(1)=lambda0;
 
-        VB(1,:)=vbar0;
+VB(1,:)=vbar0;
 
-        %energy
-        Ec=zeros(1,length(0:dt:tf));
-        Ep=zeros(1,length(0:dt:tf));
-        Ec(1)=Ec0p/2;
-        Ep(1)=Ep0;
-        DD=zeros(1,length(0:dt:tf));
-        DD(1)=D0;
-        xinitt=[x00(:,1);v00(:,1);x00(:,2);v00(:,2)];
-        [t,X]=ode23s(@csdynmatlab,0:dt:tf,xinitt);
-        X=X';
-        ftime=length(t);
-        x1=X(1:np,:);
-        x2=X(2*np+1:3*np,:);
-        v1=X(np+1:2*np,:);
-        v2=X(3*np+1:4*np,:);
+%energy
+Ec=zeros(1,length(0:dt:tf));
+Ep=zeros(1,length(0:dt:tf));
+Ec(1)=Ec0p/2;
+Ep(1)=Ep0;
+DD=zeros(1,length(0:dt:tf));
+DD(1)=D0;
+xinitt=[x00(:,1);v00(:,1);x00(:,2);v00(:,2)];
+[t,X]=ode23s(@csdynmatlab,0:dt:tf,xinitt);
+X=X';
+ftime=length(t);
+x1=X(1:np,:);
+x2=X(2*np+1:3*np,:);
+v1=X(np+1:2*np,:);
+v2=X(3*np+1:4*np,:);
 
-        col = jet(1);
-        hold on
-        d=1;
-        ip=1;
-        ipp=1;
-        dd=0;
-        sss=0;
-
-while   dd==0
-
-        s= get(handles.slider2,'Value');
-        s2=get(handles.slider7,'Value');
-        dd= get(handles.radiobutton8,'Value');%Guardar valor del boton pausa
-        ss=round(s);%ss es la variable que se genera con el slider y se redondea
-        ss2=round(s2);
-        
-if      ss==0
-        ss=1;
-        
+col = jet(1);
+hold on
+d=1;
+ip=1;
+ipp=1;
+dd=0;
+sss=0;
+% vidObj = VideoWriter('video.avi');
+% open(vidObj);
+% set(gca,'nextplot','replacechildren');
+q = 0; %contador para video
+while dd==0
+% if p==1
+s= get(handles.slider2,'Value');
+s2=get(handles.slider7,'Value');
+dd= get(handles.radiobutton8,'Value');%Guardar valor del boton pausa
+ss=round(s);%ss es la variable que se genera con el slider y se redondea
+ss2=round(s2);
+if ss==0
+ss=1;
 end
-
-if      ss2==0
-        ss2=1;
+if ss2==0
+ss2=1;
 end
+% set(handles.text2,'String',ss);
+% set(handles.text9,'String',ss2);
 
-if  sss~=ss | sss2~=ss2
-   if  d~=1
-      if  ss~=sss  
-          ip=sss; 
+%%%%% Grabacion de los agentes
+grabar = 0;
+grabar = get(handles.descargar,'Value');
+if grabar ==1
+ cla
+vidObj = VideoWriter('probando.avi');
+open(vidObj);
 
-end
-
-if      ss2~=sss2 
-        ip=sss2;
+for i = 2:q
     
-end
-    end
-        cla%borra lo que se genera anteriormente de los plot, y asi no se sobre escriben y se optimiza
-        hold on
-
-for     i=1:np
-        q = q+1;
-        xss2(q) = ss2 ;
-        xss(q)  = ss;
-        xi(q) = i;
-    
-
-        plot(x1(i,ss2:1:ss),x2(i,ss2:1:ss),'.','MarkerSize',8)
-        plot(x1(i,ss),x2(i,ss),'o','MarkerSize',8)
-   
-        axis([-2 2 -2 2])
-
+  if xss(i) < xss(i-1) %Debiera eliminar la sobreescripcion de los agentes en la parte de la cola, pero no entra al if
+   plot(x1(xi(i),xss2(i):1:xss(i)),x2(xi(i),xss2(i):1:xss(i)),'Color','white','-','MarkerSize',8)  
+  else
+         
+ plot(x1(xi(i),xss2(i):1:xss(i)),x2(xi(i),xss2(i):1:xss(i)),'.','MarkerSize',8)
   
-        sss=ss;
-        sss2=ss2;
-        d=0;
+end
+ currFrame = getframe(gcf);
+ writeVideo(vidObj,currFrame);
+ clc
+     
+    end
+  close(vidObj); 
+  gg = 1;
+  dd = 1;
+  
+
+end
+gg = 0;
+if gg == 0
+if sss~=ss | sss2~=ss2
+
+ 
+% y= -1*ss 
+% plot(ss,y,'Marker','x')
+
+if d~=1
+
+if ss~=sss  
+   ip=sss; 
+
+   %ss=sss;
+end
+if ss2~=sss2 
+   ip=sss2; 
 end
 end
-        pause(0.01)
+cla%borra lo que se genera anteriormente de los plot, y asi no se sobre escriben y se optimiza
+hold on
+%     plot(x1(2,ss2:1:ss),x2(2,ss2:1:ss),'Marker','.','Color','green')
+%     plot(x1(1,ss2:1:ss),x2(1,ss2:1:ss),'Marker','.','Color','black')
+%     plot(x1(3,ss2:1:ss),x2(3,ss2:1:ss),'Marker','.','Color','red')
+%     plot(x1(4,ss2:1:ss),x2(4,ss2:1:ss),'Marker','.','Color','blue')
+
+%%%%%%% este for esta funcional, se debe cambiar los np=nppp
 
 
+for i=1:np
+    q = q+1;
+    xss2(q) = ss2 ;
+    xss(q)  = ss;
+    xi(q) = i;
+    
+%     x11(q) = x1(i,ss2:1:ss);% probando el guardar los plot de los valores en la variable, para luego mandar a video
+%     x22(q) = x2(i,ss2:1:ss);
+    plot(x1(i,ss2:1:ss),x2(i,ss2:1:ss),'.','MarkerSize',8)
+     plot(x1(i,ss),x2(i,ss),'o','MarkerSize',8)
+   
+    axis([-2 2 -2 2])
+%     currFrame = getframe(gcf);
+%     writeVideo(vidObj,currFrame);
+%      m(i) = getframe(gcf);
+  
+sss=ss;
+sss2=ss2;
+d=0;
+ %del for de los plot originales
+end
+
+
+% grabar = 0;
+% grabar = get(handles.descargar,'Value');
+% if grabar ==1
+% vidObj = VideoWriter('video.avi');
+% open(vidObj);
+%     for i = 0:q
+%  plot(x1(xi(i),xss2(i):1:xss(i)),x2(xss2(i):1:xss(i)),'.','MarkerSize',8)
+%  currFrame = getframe(gcf);
+%  writeVideo(vidObj,currFrame);
+% 
+%      
+%     end
+%   close(vidObj); 
+% 
+% end
+end
+pause(0.01)
+
+
+
+% for i=1:np
+%     plot(x1(i,:),x2(i,:),'Color',col(i,:))
+%     plot(x1(i,1),x2(i,1),'Color',col(i,:),'Marker','x','MarkerSize',10)
+%     plot(x1(i,ftime),x2(i,ftime),'Color',col(i,:),'Marker','square','MarkerSize',10)
+% end
+
+% a=get(gca,'xlim');b=get(gca,'ylim');
+%  a=a+[-2 2];b=b+[-2 2];
+% title('Trajectories')
+
+
+end% end del if de gg
 end
 
 
