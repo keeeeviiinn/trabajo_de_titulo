72fcd3344bf1659b564e5359deb1c4c3c3790ec6
Kevin_Garcia <kevin.garcias@utem.cl>
2020-12-09 19:36:29 -0300

    Se creo nuevo GUI en java

--- Java/src/Interfaz/Interfaz01.java
+++ Java/src/Interfaz/Interfaz01.java
@@ -6,18 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JSlider;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Interfaz01 extends JFrame {
 
 	private JPanel contentPane;
-	private JButton run;
-	private JButton stop;
-	private JSlider slider;
-	private JSlider slider_1;
 
 	/**
 	 * Launch the application.
@@ -43,46 +35,8 @@ public class Interfaz01 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		{
-			run = new JButton("Run");
-			run.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					
-					
-				}
-			});
-			run.setBounds(301, 173, 85, 21);
-			contentPane.add(run);
-		}
-		
-		//funcion de csdynmatlab
-		//public static int csdynmatlab([float t,float xinit]){
-			
-			
-		//}
-		
-		{
-			stop = new JButton("Stop");
-			stop.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					
-					
-				}
-			});
-			stop.setBounds(301, 204, 85, 21);
-			contentPane.add(stop);
-		}
-		{
-			slider = new JSlider();
-			slider.setBounds(35, 173, 200, 22);
-			contentPane.add(slider);
-		}
-		{
-			slider_1 = new JSlider();
-			slider_1.setBounds(35, 204, 200, 22);
-			contentPane.add(slider_1);
-		}
 	}
+
 }
